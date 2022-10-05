dfba1278351887b1f1fc56ac7d37c09c38cbd459
Emmanuel Tolani <emmanueltolani17@gmail.com>
2022-10-05 17:14:13 -0600

    finished header prototype

---
+++ pages/index.js
@@ -0,0 +1,18 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import Footer from '../src/components/Footer'
+import Header from '../src/components/Header'
+import Hero from '../src/components/Hero'
+import MobileHeader from '../src/components/MobileHeader'
+import MainLayout from '../src/layouts/MainLayout'
+
+export default function Home() {
+  return (
+    <MainLayout>
+      {/* <MobileHeader /> */}
+      <Header />
+      {/* <Hero />
+      <Footer /> */}
+    </MainLayout>
+  )
+}
---
+++ src/components/MobileHeader.js
@@ -0,0 +1,35 @@
+import React from 'react';
+// import styled from 'styled-components';
+import PropTypes from 'prop-types';
+
+// #region constants
+
+// #endregion
+
+// #region styled-components
+
+// #endregion
+
+// #region functions
+
+// #endregion
+
+// #region component
+const propTypes = {};
+
+const defaultProps = {};
+
+/**
+ * 
+ */
+const MobileHeader = () => {
+    return <div>
+        This is the Mobile Header
+    </div>;
+}
+
+// Header.propTypes = propTypes;
+// Header.defaultProps = defaultProps;
+// #endregion
+
+export default MobileHeader;

88e13afe6fe3b4a8acf8248deb99e0d6efad164a
Emmanuel Tolani <emmanueltolani17@gmail.com>
2022-10-08 13:11:40 -0600

    upgraded question box

---
+++ src/components/Quiz/QuestionBox.js
@@ -0,0 +1,78 @@
+import React, {useState} from 'react';
+import PropTypes from 'prop-types';
+
+// #region constants
+
+// #endregion
+
+// #region styled-components
+
+// #endregion
+
+// #region functions
+
+// #endregion
+
+// #region component
+const propTypes = {};
+
+const defaultProps = {};
+
+/**
+ * 
+ */
+const QuestionBox = ({ question, options, selected, index}) => {
+   function indextoChar(index) {
+        switch(index){
+            case 0: 
+               index = 'A';
+            break; 
+            case 1: 
+               index = 'B';
+            break; 
+            case 2: 
+               index = 'C';
+            break; 
+            case 3: 
+               index = 'D';
+            break; 
+        }
+        return index
+    }
+    const [answer, setAnswer] = useState();
+    const [isActive, setIsActive] = useState(false);
+
+    // const handleClick = event => {
+    //     // toggle isActive state on click
+    //     console.log("clicked button")
+    //     setIsActive(!isActive)
+    // }
+    return(
+        <div className="question-box">
+        <h1 className="question-box__question">{index}. {question}</h1>
+        {options.map((text, index) => (
+        <div className='question-box__answers' key={index}>
+          <button
+              
+              className='answerBtn'
+              style={{ backgroundColor: index === isActive ? "#374A7C" : "" ,
+                color: index === isActive ? "white" : ""}}
+              onClick={()=>{
+                setIsActive(index)
+                let answer = text;
+                 console.log(answer)
+                   selected(answer)
+                  }}> {indextoChar(index)}
+         </button>
+         <span>{text}</span>
+         </div>
+        ),)}
+    </div>
+    );
+}
+
+QuestionBox.propTypes = propTypes;
+QuestionBox.defaultProps = defaultProps;
+// #endregion
+
+export default QuestionBox;

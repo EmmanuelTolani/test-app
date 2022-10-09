284b1d3ca42d5545e39d93993e200a6a1b2fb586
Emmanuel Tolani <emmanueltolani17@gmail.com>
2022-10-09 00:11:59 -0600

    deploy error fixing

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,9 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import Footer from '../src/components/Footer'
-import Header from '../src/components/Header'
+import Footer from '../src/components/UI/Footer'
+import Header from '../src/components/UI/Header'
 import Hero from '../src/components/Hero'
-import MobileMenu from '../src/components/MobileMenu'
+import MobileMenu from '../src/components/UI/MobileMenu'
 import Offer from '../src/components/Offer'
 import Pricing from '../src/components/Pricing'
 
